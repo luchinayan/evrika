49327a6df7af410ec45dcaf0816423f61b084d11
luchinayan <yanluchina48@gmail.com>
2025-04-12 14:12:44 +0300

    app

--- src/app/find-product/page.tsx
+++ src/app/find-product/page.tsx
@@ -1,13 +1,11 @@
-export default async function Page({ searchParams }: { searchParams: { page?: string } }) {
-  const page = parseInt(searchParams.page ?? '1', 10);
-  const limit = 10;
-
+export default async function Page() {
   const data = await fetch(`https://shop.api.onliner.by/products/xi1512512glbk/positions`);
   const mobiles = await data.json();
 
   return (
     <div>
       <ul>
+        {/*eslint-disable  @typescript-eslint/no-explicit-any */}
         {mobiles.positions.primary.map((post: any) => (
           <li key={post.id}>{post.position_price.amount}</li>
         ))}
