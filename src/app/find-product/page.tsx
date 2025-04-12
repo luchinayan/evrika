259fa835f80cb02e47e3c7d50e060f397d630434
luchinayan <yanluchina48@gmail.com>
2025-04-12 14:04:00 +0300

    app

--- src/app/find-product/page.tsx
+++ src/app/find-product/page.tsx
@@ -1,3 +1,17 @@
-export const FindProduct = () => {
-  return <div>find product</div>;
-};
+export default async function Page({ searchParams }: { searchParams: { page?: string } }) {
+  const page = parseInt(searchParams.page ?? '1', 10);
+  const limit = 10;
+
+  const data = await fetch(`https://shop.api.onliner.by/products/xi1512512glbk/positions`);
+  const mobiles = await data.json();
+
+  return (
+    <div>
+      <ul>
+        {mobiles.positions.primary.map((post: any) => (
+          <li key={post.id}>{post.position_price.amount}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
